c0597b8e64eb5c456d116b1c8428f3c73b673bb6
josiano <josianosantos07@gmail.com>
2021-06-28 16:32:54 -0300

    Refatorações

--- projeto/settings.py
+++ projeto/settings.py
@@ -1,13 +1,10 @@
 import os
 
-from decouple import config
-from dj_database_url import parse as db_url
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'zz15$l5i3a0^o@_c#lc^4e+*(^-(%@374wx9ed8f*^t(%s^lmn'
 
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', '*']
 
@@ -41,7 +38,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'oauth2_provider.middleware.OAuth2TokenMiddleware',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
@@ -67,12 +63,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'projeto.wsgi.application'
 
 DATABASES = {
-    'default': config(
-        'DATABASE_URL',
-        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
-        cast=db_url
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -103,13 +100,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'projeto/static')
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
-    #     #'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
-    #     'rest_framework.authentication.SessionAuthentication',
-    # ),
-    # 'DEFAULT_PERMISSION_CLASSES': (
-    #     'rest_framework.permissions.IsAuthenticated',
-    # )
     'DEFAULT_AUTHENTICATION_CLASSES': [],
     'DEFAULT_PERMISSION_CLASSES': [],
     'DEFAULT_PAGINATION_CLASS': 'utils.paginator.HeaderPagination',
@@ -118,14 +108,4 @@ REST_FRAMEWORK = {
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
-    #  'oauth2_provider.backends.OAuth2Backend',
 )
-
-# OAUTH2_PROVIDER = {
-#    'SCOPES': {'read': 'Read scope', 'write': 'Write scope', 'groups': 'Access to your groups'},
-#    'ACCESS_TOKEN_EXPIRE_SECONDS': 36000,
-# }
-
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
-
-DOMINIO_URL = config('DOMINIO_URL')
